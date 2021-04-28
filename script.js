7806c1cf59a9facfeaf0fa5bd05fde5c1688ad25
UltimeMan <ultimne@gmail.com>
2021-04-28 15:55:17 +0200

    nice commit to fix bug

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const img = new Image();
-img.src ='../media/flappy-bird-set.png';
+img.src ='./media/tiles.png';
 
 // paramètre
 let gamePlaying = false;
